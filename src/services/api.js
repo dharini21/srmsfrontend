7e8dad793680adcddd19670dd51413a13063f258
dharini21 <sridharini2103@gmail.com>
2026-06-28 09:06:48 +0530

    remove local http

--- src/services/api.js
+++ src/services/api.js
@@ -2,11 +2,11 @@ import axios from 'axios';
 
 const API = axios.create({
   // Change this line ↓
-baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000/api',
+baseURL: import.meta.env.VITE_API_URL,
   headers: { 'Content-Type': 'application/json' }
 });
 
-
+console.log('API URL:', import.meta.env.VITE_API_URL);
 // Attach token automatically
 API.interceptors.request.use((config) => {
   const teacher = JSON.parse(localStorage.getItem('teacher') || 'null');
